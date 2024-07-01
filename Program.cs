bb366a5b347b03fd60f3c254ea7b2f9cfda4b404
Ulugbek1904 <julugbek023@gmail.com>
2024-07-01 21:19:26 +0500

    Add Function

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClipboardGuidGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continueProg = true;
+            while (continueProg)
+            {
+                string stringGuid =
+                    Guid.NewGuid().ToString();
+
+                Console.WriteLine(stringGuid);
+                Console.WriteLine(DateTimeOffset.Now);
+
+                TextCopy.ClipboardService.SetText($"" +
+                    $"{stringGuid},{DateTimeOffset.Now.ToString()}");
+
+                Console.WriteLine("Copied!");
+                Console.WriteLine("Do you " +
+                    "want to run again?  yes(y) or any word");
+
+                string input =
+                    Console.ReadLine().ToLower();
+
+                continueProg = input == "yes"
+                    || input == "y";
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# ClipboardGuidGenerator
+# Clipboard GUID Generator
+
+This is a simple C# console application that generates a new GUID, 
+displays it along with the current date and time, and copies it 
+to the clipboard. The user is prompted 
+whether they want to generate another GUID.
+
+## Features
+
+- Generates a new GUID each time the program runs.
+- Displays the generated GUID and the current date and time.
+- Copies the GUID and date/time to the clipboard.
+- Prompts the user to run the program again.
+
+
